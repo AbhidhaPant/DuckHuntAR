d83a4f69fde9c69ab34cae65cae56c00b2ff3270
AbhidhaPant <abhidhapant@gmail.com>
2024-03-11 16:45:34 +0530

    Updated DuckHunt

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    private Transform targetfocus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetfocus = GameObject.FindGameObjectWithTag("Target").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = targetfocus.position - this.transform.position;
+        Debug.Log(target.magnitude);
+
+        if (target.magnitude < 1)
+        {
+            TargetCollider.instance.moveTarget();
+        }
+        transform.LookAt(targetfocus.transform);
+
+        float speed = Random.Range(1f, 4f);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TargetCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetCollider : MonoBehaviour
+{
+    public static TargetCollider instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        moveTarget();
+    }
+
+    public void moveTarget()
+    {
+        Vector3 temp;
+        temp.x = Random.Range(-4.5f, 4.5f);
+        temp.y = Random.Range(0f, 1f);
+        temp.z = Random.Range(-4.5f, 4.5f);
+        transform.position = new Vector3(temp.x, temp.y, temp.z);
+        if (DefaultTrackableEventHandler.trueFalse == true)
+        {
+            RaycastController.instance.playSound(0);
+        }
+    }
+}
